112f6420b5062a421f1092831bfc28eb36710a81
Omar <omsi96@users.noreply.github.com>
2020-12-29 14:38:51 +0300

    adding maps

--- components/trips/TripDetails.js
+++ components/trips/TripDetails.js
@@ -2,10 +2,42 @@ import { Image, StyleSheet, View } from "react-native";
 import { Layout, Text } from "@ui-kitten/components";
 
 import { Content } from "native-base";
+import MapView from "react-native-maps";
 import React from "react";
 
 const TripDetails = ({ navigation, route }) => {
   const trip = route.params;
+  const regionFrom = (lat, lon, distance) => {
+    try {
+      distance = distance / 2;
+      const circumference = 40075;
+      const oneDegreeOfLatitudeInMeters = 111.32 * 1000;
+      const angularDistance = distance / circumference;
+
+      const latitudeDelta = distance / oneDegreeOfLatitudeInMeters;
+      const longitudeDelta = Math.abs(
+        Math.atan2(
+          Math.sin(angularDistance) * Math.cos(lat),
+          Math.cos(angularDistance) - Math.sin(lat) * Math.sin(lat)
+        )
+      );
+
+      return {
+        latitude: lat,
+        longitude: lon,
+        latitudeDelta,
+        longitudeDelta,
+      };
+    } catch (error) {
+      return {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      };
+    }
+  };
+
   return (
     <Layout style={styles.container}>
       <Content>
@@ -15,15 +47,22 @@ const TripDetails = ({ navigation, route }) => {
         />
         <View style={[styles.contentContainer, styles.column]}>
           <Text category="h1">{trip.title}</Text>
-
-          <Image
-            style={styles.ownerImage}
-            source={{ uri: trip.profile.image }}
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              ...regionFrom(trip.lat, trip.long, 20),
+            }}
           />
-          <Text category="h5" style={styles.ownerText}>
-            {trip.profile.name}
-          </Text>
-          <Text category="p">{trip.description}</Text>
+          <View style={styles.profileInfo}>
+            <Image
+              style={styles.ownerImage}
+              source={{ uri: trip.profile.image }}
+            />
+            <Text category="h5" style={styles.ownerText}>
+              {trip.profile.name}
+            </Text>
+            <Text category="p">{trip.description}</Text>
+          </View>
         </View>
       </Content>
     </Layout>
@@ -52,12 +91,21 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: "cover",
   },
+  profileInfo: {
+    top: -65,
+
+    alignItems: "center",
+  },
   ownerImage: {
     width: 100,
     height: 100,
     borderRadius: 50,
     marginRight: 10,
     margin: 12,
+    borderStyle: "solid",
+    borderColor: "white",
+    borderWidth: 4,
+    shadowRadius: 60,
   },
   title: {
     margin: 12,
@@ -65,5 +113,10 @@ const styles = StyleSheet.create({
   ownerText: {
     marginTop: 3,
     marginBottom: 12,
+  },
+  map: {
+    width: "100%",
+    height: 400,
+    // transform: [{ scaleX: 1.1 }],
   },
 });
